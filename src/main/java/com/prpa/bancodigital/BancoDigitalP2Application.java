ee4a6efacd4c5cd3c41ce8dcbb233315c0d9f8da
Paulo Ricardo <pauloricardopaa@gmail.com>
2025-03-24 13:31:49 -0300

    Projeto criado.

---
+++ src/main/java/com/prpa/bancodigital/BancoDigitalP2Application.java
@@ -0,0 +1,13 @@
+package com.prpa.bancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDigitalP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDigitalP2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/prpa/bancodigital/BancoDigitalP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prpa.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDigitalP2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
