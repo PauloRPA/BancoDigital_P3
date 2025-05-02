c501b5ac74c8e5b11d799c23932cfa2646dfb98a
Paulo Ricardo <pauloricardopaa@gmail.com>
2025-05-02 08:08:55 -0300

    [feat] Queries sql para criação automática do banco dedados e para consulta da PoliticaTaxa adicinadas

--- src/main/resources/sql/data/politica_taxa.sql
+++ src/main/resources/sql/data/politica_taxa.sql
@@ -1,19 +1,19 @@
 insert into politica_taxa (id, nome, quantidade, tipo_taxa, unidade_quantia)
-values (1, 'Manutenção conta corrente para clientes SUPER', 8.00, 'MANUTENCAO', 'FIXO')
+values (default, 'Manutenção conta corrente para clientes SUPER', 8.00, 'MANUTENCAO', 'FIXO')
 on conflict do nothing;
 
 insert into politica_taxa (id, nome, quantidade, tipo_taxa, unidade_quantia)
-values (2, 'Manutenção conta corrente para clientes COMUM', 12.00, 'MANUTENCAO', 'FIXO')
+values (default, 'Manutenção conta corrente para clientes COMUM', 12.00, 'MANUTENCAO', 'FIXO')
 on conflict do nothing;
 
 insert into politica_taxa (id, nome, quantidade, tipo_taxa, unidade_quantia)
-values (3, 'Rendimento conta poupança para clientes PREMIUM', 0.90, 'RENDIMENTO', 'PORCENTAGEM')
+values (default, 'Rendimento conta poupança para clientes PREMIUM', 0.90, 'RENDIMENTO', 'PORCENTAGEM')
 on conflict do nothing;
 
 insert into politica_taxa (id, nome, quantidade, tipo_taxa, unidade_quantia)
-values (4, 'Rendimento conta poupança para clientes SUPER', 0.70, 'RENDIMENTO', 'PORCENTAGEM')
+values (default, 'Rendimento conta poupança para clientes SUPER', 0.70, 'RENDIMENTO', 'PORCENTAGEM')
 on conflict do nothing;
 
 insert into politica_taxa (id, nome, quantidade, tipo_taxa, unidade_quantia)
-values (5, 'Rendimento conta poupança para clientes COMUM', 0.50, 'RENDIMENTO', 'PORCENTAGEM')
+values (default, 'Rendimento conta poupança para clientes COMUM', 0.50, 'RENDIMENTO', 'PORCENTAGEM')
 on conflict do nothing;
---
+++ src/main/resources/sql/schemas/politica_taxa.sql
@@ -0,0 +1,68 @@
+-- Cria tipos enumerados no banco de dados caso não existam para os enums java tipo_taxa e unidade_taxa
+do
+ '
+begin
+    if not exists (select 1 from pg_type where typname = ''tipo_taxa'') then
+        create type tipo_taxa as enum (''MANUTENCAO'', ''RENDIMENTO'');
+    end if;
+	if not exists (select 1 from pg_type where typname = ''unidade_quantia'') then
+        create type unidade_quantia as enum (''FIXO'', ''PORCENTAGEM'');
+    end if;
+end;
+';
+
+-- Cria função para fazer o parsing das strings java para o tipo tipo_taxa definido acima
+create or replace function cast_varchar_tipo_taxa(varchar) returns tipo_taxa as '
+    select case $1
+        when ''MANUTENCAO'' then ''MANUTENCAO''::tipo_taxa
+        when ''RENDIMENTO'' then ''RENDIMENTO''::tipo_taxa
+    end;
+' language sql;
+
+-- Cria função para fazer o parsing das strings java para o tipo unidade_quantia definido acima
+create or replace function cast_varchar_unidade_quantia(varchar) returns unidade_quantia as '
+    select case $1
+        when ''FIXO'' then ''FIXO''::unidade_quantia
+        when ''PORCENTAGEM'' then ''PORCENTAGEM''::unidade_quantia
+    end;
+' language sql;
+
+-- Cria casting automático para atribuições feitas via string para o valores tipo_taxa
+do
+'
+begin
+    if not exists (
+		select 1 from pg_cast c
+		inner join pg_type t
+		on t.oid = c.casttarget
+		where t.typname = ''tipo_taxa''
+	) then
+        CREATE CAST (varchar AS tipo_taxa) WITH FUNCTION cast_varchar_tipo_taxa(varchar) AS ASSIGNMENT;
+    end if;
+end;
+';
+
+-- Cria casting automático para atribuições feitas via string para o valores unidade_taxa
+do
+'
+begin
+    if not exists (
+		select 1 from pg_cast c
+		inner join pg_type t
+		on t.oid = c.casttarget
+		where t.typname = ''unidade_quantia''
+	) then
+        CREATE CAST (varchar AS unidade_quantia) WITH FUNCTION cast_varchar_unidade_quantia(varchar) AS ASSIGNMENT;
+    end if;
+end;
+';
+
+-- Cria tabela politica_taxa
+create table if not exists politica_taxa (
+	id serial not null unique,
+	nome varchar not null unique,
+	quantidade numeric(38,2) not null,
+	tipo_taxa tipo_taxa not null,
+	unidade_quantia unidade_quantia not null,
+	constraint politica_taxa_pk primary key (id)
+);
