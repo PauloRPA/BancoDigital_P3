3a24657cc71d0343ae17804ae5b7f78aa39d79b4
Paulo Ricardo <pauloricardopaa@gmail.com>
2025-05-02 18:30:15 -0300

    [fix] Sql para inserção de dados requiridos deve ignorar conflitos.

--- src/main/resources/sql/data/politica_taxa_join_tier.sql
+++ src/main/resources/sql/data/politica_taxa_join_tier.sql
@@ -1,5 +1,5 @@
-insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (1, 2);
-insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (2, 1);
-insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (3, 3);
-insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (4, 2);
-insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (5, 1);
+insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (1, 2) on conflict do nothing;
+insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (2, 1) on conflict do nothing;
+insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (3, 3) on conflict do nothing;
+insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (4, 2) on conflict do nothing;
+insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (5, 1) on conflict do nothing;
