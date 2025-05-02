2bbe251505e1b76eda0e737e5262810a65218740
Paulo Ricardo <pauloricardopaa@gmail.com>
2025-05-02 17:38:01 -0300

    [feat] Schema, queries e dados requeridos para tabela de união entre Tier e PoliticaTaxa adicionados

---
+++ src/main/java/com/prpa/bancodigital/repository/dao/JoinTierPoliticaTaxa.java
@@ -0,0 +1,81 @@
+package com.prpa.bancodigital.repository.dao;
+
+import com.prpa.bancodigital.config.respository.QueryResolver;
+import com.prpa.bancodigital.model.PoliticaTaxa;
+import com.prpa.bancodigital.model.Tier;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JoinTierPoliticaTaxa {
+
+    public static final String POLITICA_TAXA_JOIN_TIER_TABLE_NAME = "politica_taxa_join_tier";
+    private final TierDao tierDao;
+    private final PoliticaTaxaDao politicaTaxaDao;
+    private final JdbcClient jdbcClient;
+    private final QueryResolver resolver;
+
+    public JoinTierPoliticaTaxa(TierDao tierDao, PoliticaTaxaDao politicaTaxaDao, JdbcClient jdbcClient, QueryResolver resolver) {
+        this.tierDao = tierDao;
+        this.politicaTaxaDao = politicaTaxaDao;
+        this.jdbcClient = jdbcClient;
+        this.resolver = resolver;
+    }
+
+    public List<Tier> findByPoliticaTaxaId(Long politicaTaxaId) {
+        return jdbcClient.sql(resolver.get(tierDao.getTableName(), "findByPoliticaTaxaId"))
+                .param("id", politicaTaxaId)
+                .query(tierDao.getRowMapper())
+                .list();
+    }
+
+    public List<PoliticaTaxa> findByTierId(Long tierId) {
+        return jdbcClient.sql(resolver.get(politicaTaxaDao.getTableName(), "findByTierId"))
+                .param("id", tierId)
+                .query(politicaTaxaDao.getRowMapper())
+                .list();
+    }
+
+    private void deleteByPoliticaTaxaId(long politicaTaxaId) {
+        jdbcClient.sql(resolver.get(getTableName(), "deleteByPoliticaTaxaId"))
+                .param("id", politicaTaxaId)
+                .update();
+    }
+
+    private void deleteByTierId(long tierId) {
+        jdbcClient.sql(resolver.get(getTableName(), "deleteByTierId"))
+                .param("id", tierId)
+                .update();
+    }
+
+    public void removeReferencesForTier(long id) {
+        Optional<Tier> byId = tierDao.findById(id);
+        if (byId.isEmpty()) return;
+        deleteByTierId(id);
+    }
+
+    public void removeReferencesForPoliticaTaxa(long id) {
+        Optional<PoliticaTaxa> byId = politicaTaxaDao.findById(id);
+        if (byId.isEmpty()) return;
+        deleteByPoliticaTaxaId(id);
+    }
+
+    public void insertReferencesForPoliticaTaxa(Long savedId, PoliticaTaxa toSave) {
+        for (Tier tier : toSave.getTiers()) {
+            if (tier.getId() != null && tierDao.findById(tier.getId()).isPresent()) {
+                jdbcClient.sql(resolver.get(getTableName(), "insert"))
+                        .param("tier_id_fk", tier.getId())
+                        .param("politica_taxa_id_fk", savedId)
+                        .update();
+            }
+        }
+    }
+
+    private String getTableName() {
+        return POLITICA_TAXA_JOIN_TIER_TABLE_NAME;
+    }
+
+}
---
+++ src/main/resources/sql/data/politica_taxa_join_tier.sql
@@ -0,0 +1,5 @@
+insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (1, 2);
+insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (2, 1);
+insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (3, 3);
+insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (4, 2);
+insert into politica_taxa_join_tier (politica_taxa_id_fk, tier_id_fk) values (5, 1);
